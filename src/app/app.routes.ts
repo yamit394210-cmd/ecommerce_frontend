19f11b9c8ac7ba4d0a7de390c5d7c7a9c0eaf3c9
yamit394210-cmd <yamit394210@gmail.com>
2026-01-10 17:13:08 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Customers } from './customers/customers';
+import { Categories } from './categories/categories';
+import { Dashboard } from './dashboard/dashboard';
+import { Orders } from './orders/orders';
+import { Products } from './products/products';
+import { Users } from './users/users';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:Login}, 
+    {path:"customers", component:Customers}, 
+    {path:"categories", component:Categories},
+    {path:"dashboard", component:Dashboard},
+    {path:"users", component:Users},
+    {path:"orders", component:Orders},
+    {path:"products", component:Products}
+]
